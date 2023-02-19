fb746ff7e0d76944101845e994169d2dc5129f27
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-19 19:52:49 +0000

    changed ami

--- terraform-c1/c2-ec2instance.tf
+++ terraform-c1/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "myec2instance" {
-    ami = "ami-099435c28ebc71f84"
+    ami = "ami-0dfcb1ef8550277af"
     instance_type = "t3.micro"
     user_data = file("${path.module}/app1/app1-install.sh")
     tags = {
