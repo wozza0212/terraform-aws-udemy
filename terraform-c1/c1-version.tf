60152c6a34ea6b68826ea7c68e1481e0dd0fc247
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-19 18:36:28 +0000

    end of video 19

---
+++ terraform-c1/c1-version.tf
@@ -0,0 +1,11 @@
+#Terraform block
+terraform {
+    required_versions = "1.3.7"
+    required_providers {
+        aws = {
+            source = "value"
+            version = "value"
+        }
+
+    }
+}
